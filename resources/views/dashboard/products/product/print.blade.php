8cfa825d1e2411f1155231bd0a87554593cd2f8f
Israa <info@schemecode.com>
2022-03-02 11:57:38 +0200

    edit printing

--- resources/views/dashboard/products/product/print.blade.php
+++ resources/views/dashboard/products/product/print.blade.php
@@ -10,10 +10,10 @@
                 margin: 0;
             }
             @page {
-                /* size: 35mm 25mm;  */
+                /* size: 20mm 10mm;  */
                 /* margin: 25mm 25mm 25mm 25mm;   */
                 /* margin: 0;*/
-                size: A4 landscape;
+                /* size: A4 landscape; */
                 /* size: auto;    */
 
                 /* this affects the margin in the printer settings */ 
@@ -44,7 +44,7 @@
     </style>
 @endpush
 @section('content')
-<div id = "content_here" style = "display:inline-block;text-align:center" >
+<div id = "content_here" style = "" >
     @foreach($products as $product)
     <div style="page-break-after: always;margin:0;" class = "page">
         <div> 
@@ -60,38 +60,40 @@
             {{-- DNS1D::getBarcodeHTML('9780691147727', 'EAN13' , 0.7 , 40 , 'black', true)--}}
             {{--<img src = "{{asset(DNS1D::getBarcodePNGPath('2016177069120', 'EAN13' , 2 , 50 , array(0 , 0 , 0) , true))}}">--}}
             {{-- {!! DNS1D::getBarcodeSVG($product->prod_code, 'EAN13',2.1,100,'black', true) !!} --}}
-            {!! DNS1D::getBarcodeSVG($product->prod_code, 'EAN13',2.8,100,'black', true) !!}
+            {!! DNS1D::getBarcodeSVG($product->prod_code, 'EAN13',2.1,60,'black', true) !!}
             {{-- DNS1D::getBarcodeHTML('9780691147727', 'C39+')--}}
 
             
 
         </div>
 
-        <span class = "" style = "display:inline-block;margin-right:50px;">M.O.M Brand</span>
+        <span class = "" style = "display:block;margin-right:50px;">M.O.M Brand</span>
+
     </div>
-</div>
     @endforeach
+</div>
+    
 @endsection
 
 @push('scripts')
 <script src = "{{asset('print.min.js')}}"></script>
 <script>
-    function printDiv() {
+    /*function printDiv() {
             var divContents = document.getElementById("content_here").innerHTML;
-            var a = window.open('', '', 'height=100, width=100');
+            var a = window.open('', '');
             a.document.write('<html>');
             a.document.write('<body>');
             a.document.write(divContents);
             a.document.write('</body></html>');
             a.document.close();
             a.print();
-        }
-    // window.print()
+        }*/
+     //window.print()
     //printDiv();
     //printJS('content_here', 'html')
     printJS({printable: 'content_here',
              type: 'html',
-             maxWidth: '500',
+            // maxWidth: '500',
               
               })
 </script>
